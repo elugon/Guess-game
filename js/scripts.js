3cbe9ae247b11fd8232f215f95c20f175ffb70f8
Elu Gonzalez <elugon10@gmail.com>
2022-05-12 20:13:01 +0200

    correcciones realizadas en el css

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
 window.onload = function () {
-  const canvas = document.getElementById('canvas');
-    const startPage = document.getElementById('start-page');
+      const startPage = document.getElementById('start-page');
   const gamePage=document.getElementById('gameScreen')
   const startButton = document.getElementById('start');
   
   startButton.onclick = function () {
     startPage.style = "display: none";
     gamePage.style="display:flex"
-    canvas.classList.remove('hidden');
     const game=new Game();
     game.start()
   }
